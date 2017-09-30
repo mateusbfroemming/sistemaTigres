67eee82bc28c133ff6aea6134b2c88b81a4396b2
Mateus <Mateus@Mateus-PC>
2017-09-29 22:28:57 -0300

    primeiro commit

--- Tigres/src/util/HibernateUtil.java
+++ Tigres/src/util/HibernateUtil.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package util;
 
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.SessionFactory;
 
-/**
- * Hibernate Utility class with a convenient method to get Session Factory
- * object.
- *
- * @author Mateus
- */
+
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory;
